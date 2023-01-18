a16e5b3bd2993ddcc205fee16b7936190a9627bb
Your Name <you@example.com>
2023-01-18 17:48:13 -0500

    Added JS file for questions, Tweaked HTML file, created highscore JS and HTML file. Can't figure out why my highscore page won't submit. Getting and missing file error

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,13 +1,31 @@
-// vars
-let highScoresText = document.querySelector("#high-scores");
-let highScoresTag = '';
-let initialsText = document.querySelector("#initials");
+function printHighscores() {
+    // either get scores from localstorage or set to empty array
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+    // sort highscores in descending order
+    highscores.sort(function(a, b) {
+      return b.score - a.score;
+    });
+  
+    highscores.forEach(function(score) {
+      // will create an li tag for each highscore
+      var liTag = document.createElement("li");
+      liTag.textContent = score.initials + " - " + score.score;
+  
+      // will display value on page
+      var olEl = document.getElementById("highscores");
+      olEl.appendChild(liTag);
+    });
+  }
+  
+  function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+  }
+  
+  document.getElementById("clear").onclick = clearHighscores;
+  
+  // will run the function once page is loaded
+  printHighscores();
 
-// High score list creation
-for (let i = 0; i < localStorage.length; i++) {
-highScoreData = JSON.parse(localStorage.getItem(i+1));
-highScoresTag = highScoresTag.concat('</br><div id="high-scores">'+ highScoreData.initials + " "+"-"+" " + highScoreData.score +'</div>');
-}
-
-highScoresText.innerHTML = highScoresTag;
 
--- assets/js/script2.js
+++
@@ -1,7 +0,0 @@
-let clearBtn = document.querySelector("#clear-score");
-
-clearBtn.onclick = () => {
-    // Clear local storage
-    localStorage.clear();
-    location.reload();
-}
